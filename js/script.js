d9959dc61e49de5077450afdb14f1ed9791ae6c8
Andre Gonçalves <dregoncalves22@gmail.com>
2024-07-08 11:41:57 -0300

    fix: arrumando regra de acentos e letras maiúsculas

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 document.querySelector('.btn-criptografar').addEventListener('click', () => {
     const inputText = document.getElementById('text-area').value;
-    const result = encriptar(inputText);
+    const result = inputText.trim() === "" ? "" : encriptar(inputText);
     mostrarResultado(result, inputText);
 });
 
 document.querySelector('.btn-descriptografar').addEventListener('click', () => {
     const inputText = document.getElementById('text-area').value;
-    const result = desencriptar(inputText);
+    const result = inputText.trim() === "" ? "" : desencriptar(inputText);
     mostrarResultado(result, inputText);
 });
 
+// Função para verificar se a string contém apenas letras minúsculas e sem acento
+function contemApenasMinusculasSemAcento(text) {
+    return /^[a-z\s]*$/.test(text);
+}
+
+// Função para criptografar o texto
 function encriptar(text) {
+    if (!contemApenasMinusculasSemAcento(text)) {
+        return "Erro: O texto deve conter apenas letras minúsculas e sem acentos.";
+    }
+
     return text.replace(/e/g, 'enter')
                .replace(/i/g, 'imes')
                .replace(/a/g, 'ai')
@@ -18,7 +28,12 @@ function encriptar(text) {
                .replace(/u/g, 'ufat');
 }
 
+// Função para descriptografar o texto
 function desencriptar(text) {
+    if (!contemApenasMinusculasSemAcento(text)) {
+        return "Erro: O texto deve conter apenas letras minúsculas e sem acentos.";
+    }
+
     return text.replace(/enter/g, 'e')
                .replace(/imes/g, 'i')
                .replace(/ai/g, 'a')
@@ -26,15 +41,21 @@ function desencriptar(text) {
                .replace(/ufat/g, 'u');
 }
 
-function mostrarResultado(result) {
+// Função para mostrar o resultado na tela
+function mostrarResultado(result, inputText) {
     const resultContainer = document.querySelector('.result-container');
 
-    if (result === "") {
+    if (inputText.trim() === "") {
         resultContainer.innerHTML = `
             <img src="img/result-img.svg" alt="Garoto usando uma lupa">
             <div class="no-result-text">
                 <h2>Nenhuma mensagem encontrada</h2>
                 <p>Digite um texto que você deseja criptografar ou descriptografar.</p>
+            </div>`;
+    } else if (result.startsWith("Erro")) {
+        resultContainer.innerHTML = `
+            <div class="result-text">
+                <p>${result}</p>
             </div>`;
     } else {
         resultContainer.innerHTML = `
@@ -51,6 +72,7 @@ function mostrarResultado(result) {
     }
 }
 
+// Função para copiar o texto para a área de transferência
 function copiarTexto(text) {
     const textarea = document.createElement('textarea');
     textarea.value = text;
